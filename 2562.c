184a1e6f7c098877d1c89fad08bbcc35f1c862db
miingjeong <alswjd7461@gmail.com>
2020-12-25 17:23:23 +0900

    최댓값

---
+++ 2562.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+#include <stdio.h>
+#define SIZE 9
+
+int main(void)
+{
+    int i;
+    int max = 0;
+    int num[SIZE];
+    for (i = 0; i < SIZE; i++) {
+        scanf("%d", &num[i]);
+    }
+
+    for (i = 0; i < SIZE; i++) {
+        if (max < num[i]) {
+            max = num[i];
+        }
+    }
+
+    printf("%d\n", max);
+    
+    for (i = 0; i < SIZE; i++)
+        if (max == num[i])
+            printf("%d", i + 1);
+  
+ 
+    return 0;
+
+
+}
+
+
