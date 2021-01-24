b81e53f7274c9050a977ddbf1aa5216925d7b5dd
Minjeong Kim <63052461+miingjeong@users.noreply.github.com>
2021-01-24 23:31:02 +0900

    Add files via upload

---
+++ 2440.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h> 
+
+int main() {
+	int n;
+	int i, j;
+	scanf("%d", &n);
+
+	for (i = 0;i<n; i++) {
+		for (j = n-i; j > 0; j--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
